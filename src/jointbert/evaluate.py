bce10a44e3f7a6901d3b8b1a775e60e3fb0e9933
Aleksandr SHVETS <ashvets@ohpc.s.upf.edu>
2022-06-08 20:04:51 +0200

    minor change

--- src/jointbert/evaluate.py
+++ src/jointbert/evaluate.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     results_mode = sys.argv[2]  # test or dev
 
     out_slot_label_list = json.load(
-        open(os.path.join(results_path, "out_slot_label_list_%s_1.json" % (results_mode)), "r"))
-    slot_preds_list = json.load(open(os.path.join(results_path, "slot_preds_list_%s_1.json" % (results_mode)), "r"))
-    with open(os.path.join(results_path, "classification_report_%s_1.txt" % (results_mode)), "w") as fout:
+        open(os.path.join(results_path, "out_slot_label_list_%s_0.json" % (results_mode)), "r"))
+    slot_preds_list = json.load(open(os.path.join(results_path, "slot_preds_list_%s_0.json" % (results_mode)), "r"))
+    with open(os.path.join(results_path, "classification_report_%s_0.txt" % (results_mode)), "w") as fout:
         fout.write(classification_report(out_slot_label_list, slot_preds_list, digits=4))
     print(precision_score(out_slot_label_list, slot_preds_list))
     print(recall_score(out_slot_label_list, slot_preds_list))
